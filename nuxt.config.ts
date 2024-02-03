d5fb188a2b76470b8b6cf0b647a1e6be458076a2
DesiredName <wc3-enter@yandex.ru>
2024-02-03 13:24:02 +0100

    'update'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@
 export default defineNuxtConfig({
     css: ['./assets/scss/index.scss'],
     devtools: { enabled: true },
-    modules: ['@nuxtjs/color-mode'],
+    modules: [
+        '@nuxtjs/color-mode',
+        [
+            '@nuxtjs/google-fonts',
+            {
+                families: {
+                    Roboto: { wght: [100, 400, 700] },
+                },
+                display: 'swap',
+                preconnect: true,
+            },
+        ],
+    ],
 });
