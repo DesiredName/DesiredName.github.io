deeb97c132cfa451918f009333bfada014983058
DesiredName <wc3-enter@yandex.ru>
2024-04-16 09:54:15 +0200

    commit

--- sw.js
+++ sw.js
@@ -1,84 +1,89 @@
-const CACHE_ID = 69;
+const CACHE_ID = 81;
 const OFFLINE_URL = '/no-connection.html';
-let req_id = 0;
 
-console.log(`starting service worker ${CACHE_ID}`);
+console.log(`starting service worker #${CACHE_ID}`);
 
-self.addEventListener('install', () => {
-    self.skipWaiting();
+async function precache() {
+    const cache = await caches.open(CACHE_ID);
+
+    return cache.addAll([
+        '/',
+        '/index.html',
+        '/site.webmanifest',
+        OFFLINE_URL,
+        '/assets/colors.css',
+        '/assets/elements-common.css',
+        '/assets/github-mark-white.svg',
+        '/assets/leetcode.png',
+        '/assets/LI-In-Bug.png',
+        '/assets/style.css',
+        '/components/the-post/index.mjs',
+        '/components/the-post/style.css',
+        '/android-chrome-144x144.png',
+        '/android-chrome-192x192.png',
+        '/android-chrome-512x512.png',
+        '/apple-touch-icon.png',
+        '/favicon.ico',
+        '/favicon-32x32.png',
+        '/favicon-16x16.png',
+    ]);
+}
+
+async function stale_with_revalidate(request, is_offline_page) {
+    try {
+        const from_cache = caches.match(request);
+        const from_network = fetch(request).then(async (res) => {
+            if (res.ok) {
+                const clone = res.clone();
+
+                caches.open(CACHE_ID).then((cache) => {
+                    cache.put(request, clone);
+                });
+            }
+
+            return res;
+        });
+
+        return (await from_cache) || (await from_network);
+    } catch (ex) {
+        if (request.mode === 'navigate') {
+            if (is_offline_page) {
+                return Response.error();
+            } else {
+                return Response.redirect(OFFLINE_URL);
+            }
+        } else {
+            return Response.error();
+        }
+    }
+}
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(precache());
 });
 
 self.addEventListener('activate', (e) => {
     e.waitUntil(
         caches
             .keys()
-            .then((keys) => keys.forEach((key) => caches.delete(key)))
-            .then(() => caches.open(CACHE_ID))
-            .then((cache) =>
-                cache.addAll([
-                    '/',
-                    '/index.html',
-                    '/site.webmanifest',
-                    OFFLINE_URL,
-                    '/assets/colors.css',
-                    '/assets/elements-common.css',
-                    '/assets/github-mark-white.svg',
-                    '/assets/leetcode.png',
-                    '/assets/LI-In-Bug.png',
-                    '/assets/style.css',
-                    '/components/the-post/index.mjs',
-                    '/components/the-post/style.css',
-                    '/android-chrome-144x144.png',
-                    '/android-chrome-192x192.png',
-                    '/android-chrome-512x512.png',
-                    '/apple-touch-icon.png',
-                    '/favicon.ico',
-                    '/favicon-32x32.png',
-                    '/favicon-16x16.png',
-                ]),
+            .then((keys) =>
+                keys.forEach((key) => {
+                    key !== CACHE_ID ? caches.delete(key) : null;
+                }),
             )
-            .then(() => clients.claim()),
+            .then(() => clients.claim())
+            .then(() => self.skipWaiting()),
     );
 });
 
 self.addEventListener('fetch', (e) => {
     const request = e.request;
     const url = new URL(request.url);
+    const is_offline_page = url.pathname.endsWith(OFFLINE_URL);
 
-    async function lookup() {
-        const id = req_id++;
-        const cached = await caches.match(request);
-
-        console.log(`${id}: requesting "${url}"`);
-
-        if (cached != null) {
-            console.log(`${id}: found in cache`);
-            return cached;
-        } else {
-            const signal = AbortSignal.timeout(5000);
-
-            return fetch(request, { signal })
-                .then((res) => {
-                    console.log(
-                        `${id}: request resolved with network [${res.ok}]`,
-                    );
-                    return res;
-                })
-                .catch((error) => {
-                    console.log(`${id}: request failed "${error}"`);
-                    if (
-                        request.mode === 'navigate' &&
-                        !url.pathname.endsWith(OFFLINE_URL)
-                    ) {
-                        console.log(`${id}: redirect to offline page`);
-                        return Response.redirect(OFFLINE_URL);
-                    } else {
-                        console.log(`${id}: return error response`);
-                        return Response.error();
-                    }
-                });
-        }
+    if (/^https?:/.test(url.protocol) !== true) {
+        return;
     }
 
-    e.respondWith(lookup());
+    e.respondWith(stale_with_revalidate(request, is_offline_page));
 });
