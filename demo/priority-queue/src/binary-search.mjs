4953da9fe1bb5cc9f4bd4903c84f4d6ce0a6190a
DesiredName <wc3-enter@yandex.ru>
2024-04-30 02:06:58 +0200

    update

--- demo/priority-queue/src/binary-search.mjs
+++ demo/priority-queue/src/binary-search.mjs
@@ -6,7 +6,7 @@ export default function BS(queue, prop_name, target_value) {
             const mid = Math.floor((l + r) / 2);
             const value = queue[mid][prop_name];
 
-            if (value > target_value) {
+            if (value >= target_value) {
                 return lookup(l, mid);
             } else {
                 return lookup(mid + 1, r);
